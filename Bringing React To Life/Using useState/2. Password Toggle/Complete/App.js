05860685cc2f67f1b6ee7ceafcea766689018e4a
Tyler Olli <ollityler@gmail.com>
2023-08-16 18:25:45 -0400

    Using useState - Password Toggle challenge

---
+++ Bringing React To Life/Using useState/2. Password Toggle/Complete/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+function PasswordInput({ minimum = 8 }) {
+  const [inputValue, setInputValue] = React.useState("");
+  const [isInputValueVisible, setIsInputValueVisible] = React.useState(false);
+  const thresholdMet = inputValue.length >= minimum;
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleToggleVisibility = () => {
+    setIsInputValueVisible(!isInputValueVisible);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (thresholdMet) {
+      alert("Password submitted");
+    } else {
+      alert("You need a longer password");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="limited-text-input">Password:</label>
+        <span className={thresholdMet ? "no-error" : "error"}>
+          {inputValue.length}
+        </span>
+      </div>
+      <div>
+        <input
+          placeholder="Enter a password"
+          type={isInputValueVisible ? "text" : "password"}
+          id="limited-text-input"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button type="button" onClick={handleToggleVisibility}>
+          {isInputValueVisible ? "🙊" : "🙈"}
+        </button>
+      </div>
+
+      <button type="submit" className="primary">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default PasswordInput;
---
+++ Bringing React To Life/Using useState/2. Password Toggle/Starter/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function PasswordInput({ minimum = 8 }) {
+  const inputValue = "";
+  const isInputValueVisible = false;
+  const thresholdMet = false;
+
+  const handleChange = () => {};
+
+  const handleToggleVisibility = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (thresholdMet) {
+      alert("Password submitted");
+    } else {
+      alert("You need a longer password");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="limited-text-input">Password:</label>
+        <span className={thresholdMet ? "no-error" : "error"}>
+          {inputValue.length}
+        </span>
+      </div>
+      <div>
+        <input
+          placeholder="Enter a password"
+          type={isInputValueVisible ? "text" : "password"}
+          id="limited-text-input"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button type="button" onClick={handleToggleVisibility}>
+          {isInputValueVisible ? "🙊" : "🙈"}
+        </button>
+      </div>
+
+      <button type="submit" className="primary">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default PasswordInput;
