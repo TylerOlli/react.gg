f6a394cddb837368cb217be4b55cb0eb36efd9ac
Tyler Olli <ollityler@gmail.com>
2023-08-16 16:32:12 -0400

    JSX - Ternary Rendering exercise

---
+++ Describing UI/JSX/5. Ternary Rendering/Starter/App.js
@@ -0,0 +1,31 @@
+import { getIsLactoseTolerant } from "./utils";
+
+function LactoseIntolerant() {
+  return (
+    <h1>
+      <span role="img" aria-label="broccoli and strawberry">
+        🥦🍓
+      </span>
+    </h1>
+  );
+}
+
+function LactoseTolerant() {
+  return (
+    <h1>
+      <span role="img" aria-label="milk and cheese">
+        🥛🧀
+      </span>
+    </h1>
+  );
+}
+
+export default function App() {
+  const isLactoseTolerant = getIsLactoseTolerant();
+
+  if (isLactoseTolerant) {
+    return <LactoseTolerant />;
+  }
+
+  return <LactoseIntolerant />;
+}
